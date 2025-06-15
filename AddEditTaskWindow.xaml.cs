b23d10369c384b4fc1257efad002c460b4200f2c
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-06-15 10:38:33 -0400

    Add project files.

---
+++ AddEditTaskWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Personal_Task_Manager
+{
+    /// <summary>
+    /// Interaction logic for AddEditTaskWindow.xaml
+    /// </summary>
+    public partial class AddEditTaskWindow : Window
+    {
+        public AddEditTaskWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
